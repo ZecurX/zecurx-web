599d374d24cd1f9eaae2f79bdf66d1ad245d2c08
shreyaShetty <shreyashetty8d@gmail.com>
2026-07-07 12:19:44 +0530

    safe acceptance of svg

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,7 @@ const nextConfig: NextConfig = {
   ],
   images: {
     dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'none'; script-src 'none'; sandbox;",
     qualities: [25, 50, 75],
     formats: ["image/webp"],
     minimumCacheTTL: 2678400, // 31 days
